611b9bf33b13b50695f0d58e3fa04c0b44def0cb
Nichita Kiselciuk <nichita.kiselciuk@gmail.com>
2020-05-11 20:25:12 +0300

    edik loh

--- src/main/java/com/proj/model/LegacySensorsXml.java
+++ src/main/java/com/proj/model/LegacySensorsXml.java
@@ -5,9 +5,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class LegacySensorsXml {
@@ -17,18 +15,20 @@ public class LegacySensorsXml {
     HumidityPercent humidity_percent;
     @JsonProperty("temperature_celsius")
     TemperatureCelsius temperature_celsius;
-    public static class HumidityPercent{
+
+    public static class HumidityPercent {
         @JsonProperty("value")
-        @JacksonXmlElementWrapper(useWrapping=false)
+        @JacksonXmlElementWrapper(useWrapping = false)
         List<Double> value;
 
         public List<Double> getValue() {
             return value;
         }
     }
-    public static class TemperatureCelsius{
+
+    public static class TemperatureCelsius {
         @JsonProperty("value")
-        @JacksonXmlElementWrapper(useWrapping=false)
+        @JacksonXmlElementWrapper(useWrapping = false)
         List<Double> value;
 
         public List<Double> getValue() {
@@ -39,7 +39,7 @@ public class LegacySensorsXml {
     public static void main(String[] args) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         String str = "{\"message\": \"<SensorReadings unix_timestamp_100us='15886868197961'>    <humidity_percent>        <value>81.8625687837649</value>        <value>64.17303405893723</value>    </humidity_percent>    <temperature_celsius>        <value>4.812248650292794</value>        <value>7.600272223055896</value>    </temperature_celsius></SensorReadings>\"}";
-        LegacySensors sensors = mapper.readValue(str,LegacySensors.class);
+        LegacySensors sensors = mapper.readValue(str, LegacySensors.class);
 
         XmlMapper mapper1 = new XmlMapper();
         System.out.println(mapper.writeValueAsString(mapper1.readValue(sensors.message, LegacySensorsXml.class)));
